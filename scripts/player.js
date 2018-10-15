f1bb7eda43fe4c80311d4dcf8f16e247df1b51d4
JM Cléry <jmclery@gmail.com>
2018-10-15 15:00:03 +0200

    le joueur peut tirer

--- scripts/player.js
+++ scripts/player.js
@@ -11,7 +11,8 @@ function createPlayer() {
             offsetY : 3,
             width : 26,
             height : 16
-        }
+        },
+        bullet : null
     };
 
     return player;
@@ -35,6 +36,29 @@ function animatePlayer() {
         player.x = canvas.width - player.sprite.width;
     }
 
+    // Si le joueur tire
+    if (Keyboard.SPACE) {
+        if (player.bullet === null) {
+            player.bullet = {
+                x : player.x + player.sprite.width / 2 - 2,
+                y : player.y,
+                width : 4,
+                height : 15,
+                color : '#0f0',
+                speed : 9
+            };
+        }
+    }
+
+    // Etat d'avancement du shoot joueur
+    if (player.bullet !== null) {
+        player.bullet.y -= player.bullet.speed;
+
+        if (player.bullet.y + player.bullet.height < 0) {
+            player.bullet = null;
+        }
+    }
+
 }
 
 function renderPlayer() {
@@ -52,4 +76,15 @@ function renderPlayer() {
         player.sprite.width,
         player.sprite.height
     );
+
+    // Dessin du shoot joueur
+    if (player.bullet !== null) {
+        context.fillStyle = player.bullet.color;
+        context.fillRect(
+            player.bullet.x,
+            player.bullet.y,
+            player.bullet.width,
+            player.bullet.height
+        );
+    }
 }
